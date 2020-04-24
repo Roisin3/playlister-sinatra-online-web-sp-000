01dde97166bc92f7688f1b3beeaca552f3a27ad8
Michael Lynch <michael@michaelrlynch.com>
2020-04-24 17:23:39 +0000

    Automatically backed up by Learn

--- app/controllers/songs_contoller.rb
+++ app/controllers/songs_contoller.rb
@@ -21,9 +21,24 @@ class SongsController < ApplicationController
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
-    @song.save 
+    @song.save
 
     rediect("/songs/#{@song.slug}")
+
+    get "/songs/:slug/edit" do
+      @song= Song.find_by_slug(params[:slug])
+      erb :'songs/edit'
+    end
+
+    patch '/songs/:slug/edit' do
+      @song = Song.find_by_slug(params[:slug])
+      @song.update(params[:song])
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+      @song.genre_ids = params[:genres]
+      @song.save
+
+      redirect("/songs/#{@song.slug}")
+    end
   end
 
 
