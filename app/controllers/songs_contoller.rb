a14d349c16861cbbc5540d2e7edcd51caf73eddb
Michael Lynch <michael@michaelrlynch.com>
2020-04-24 17:14:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_contoller.rb
+++ app/controllers/songs_contoller.rb
@@ -21,7 +21,7 @@ class SongsController < ApplicationController
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
-    @song.save
+    @song.save 
 
     rediect("/songs/#{@song.slug}")
   end
